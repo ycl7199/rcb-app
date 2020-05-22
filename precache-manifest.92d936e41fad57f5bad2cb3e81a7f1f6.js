42261b7d67a924787204f81ec01313ad5fe8ff97
劉益誠 <kevin861006@gmail.com>
2020-05-22 16:57:39 +0800

    Updates

---
+++ precache-manifest.92d936e41fad57f5bad2cb3e81a7f1f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34abf065ef2dac438e90e811e5de1fc",
+    "url": "/rcb-app/index.html"
+  },
+  {
+    "revision": "4dd1076440668dd72301",
+    "url": "/rcb-app/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "56494f68283018e579e7",
+    "url": "/rcb-app/static/css/main.63dfd3ab.chunk.css"
+  },
+  {
+    "revision": "4dd1076440668dd72301",
+    "url": "/rcb-app/static/js/2.e47da491.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rcb-app/static/js/2.e47da491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56494f68283018e579e7",
+    "url": "/rcb-app/static/js/main.7748873a.chunk.js"
+  },
+  {
+    "revision": "a40c1a68b300e9348ef9",
+    "url": "/rcb-app/static/js/runtime-main.90ccada4.js"
+  }
+]);
